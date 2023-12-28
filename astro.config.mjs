80ce3cca0003e7dd3dc75b02b1a2c242784af389
Marc RDWZ <redwerkz@gmail.com>
2023-12-28 02:28:47 +0100

    Add `astro-compress`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import compress from "astro-compress";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.VERCEL_ENV === 'production' ? 'https://astro.badg.es/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'http://localhost:4321/',
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind({ applyBaseStyles: false }),compress()],
   vite: {
     ssr: { external: ['@resvg/resvg-js'] },
     optimizeDeps: { exclude: ['@resvg/resvg-js'] },
